9b429f8c727d8eb6fe82bd4e1f69dbeb9445930c
shiotsuki40 <katsuraaa7575@gmail.com>
2023-02-18 13:55:33 +0900

    追加 button history soucommit sun tree

---
+++ my_app/src/History.js
@@ -0,0 +1,14 @@
+import React from "react";
+import HistoryTreeSp from "./HistoryTreeSp";
+import HistoryButton from "./HistoryButton";
+function History() {
+  return (
+    <div>
+      <h1>History</h1>
+      <HistoryButton />
+      <HistoryTreeSp />
+    </div>
+  );
+}
+
+export default History;
---
+++ my_app/src/Soucommit.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Soucommit.css";
+function Soucommit() {
+  // let commit ;
+
+  ////////////////////////////////メインツリーと同じ方法でコッミト数を表示する////////////////////////////////
+  return (
+    <>
+      <p className="soucommit">
+        今月の総コミット数『〇』 今週のコミット数『〇』(先週のコミット数『〇』)
+        アイテム数『〇』
+      </p>
+    </>
+  );
+}
+
+export default Soucommit;
