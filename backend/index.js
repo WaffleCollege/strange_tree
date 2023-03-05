ac90887ffff1294a2e9693a0f15c3a149033d84e
nachan28 <torajiro282828@gmail.com>
2023-03-05 15:51:30 +0900

    追加: express-sessionをインストール

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const path = require("path");
+const expressSession = require('express-session');
 
 app.use(express.static(path.join(__dirname, "..", "my_app", "build")));
 app.use((req, res, next) => {
