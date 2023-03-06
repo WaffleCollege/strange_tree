7365f87e18f44eb3045bf9d9d1b5adf17a4de7c3
nachan28 <torajiro282828@gmail.com>
2023-03-06 18:34:16 +0900

    追加: UIサーバを追加

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "..", "my_app", "build")));
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, "..", "my_app", "build", "index.html"))
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
