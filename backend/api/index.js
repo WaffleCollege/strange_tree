8bb0a9686ce088c3ab41981a9128c34c1d2315c5
nachan28 <torajiro282828@gmail.com>
2023-03-07 14:07:34 +0900

    追加: 初回ログイン時にユーザ情報をUsersテーブルにポストするコード追加

--- backend/api/index.js
+++ backend/api/index.js
@@ -5,24 +5,41 @@ const path = require("path");
 const pool = require("./db");
 const cors = require("cors");
 
-app.use(cors({
-  origin: 'http://localhost:3000', 
-  credentials: true, 
-  optionsSuccessStatus: 200 
-}))
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+    optionsSuccessStatus: 200,
+  })
+);
 app.use(express.json());
 
 app.get("/tree", (req, res) => {
   pool.query("select * from tree", (error, results) => {
-    if(error) throw error;
+    if (error) throw error;
     return res.status(200).json(results.rows);
-  })
-})
+  });
+});
 
 app.post("/users", (req, res) => {
-  const username = req.body.username;
-  pool.query(`insert into users (username, email, avatar, first_login) values `)
-})
+  const { username, email, avatar, first_login, tree_id } = req.body;
+  pool.query(
+    "select * from users where username = $1 and email = $2",
+    [username, email],
+    (error, results) => {
+      if (!results.rows.length) {
+        pool.query(
+          `insert into users (username, email, avatar, first_login) values ($1, $2, $3, $4)`,
+          [username, email, avatar, first_login],
+          (error, results) => {
+            if (error) throw error;
+            return res.status(201).send("registered!");
+          }
+        );
+      }
+    }
+  );
+});
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
