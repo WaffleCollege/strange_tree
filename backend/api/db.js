526e554e935bdbe7b4c21381a089f6b8701e39c0
nachan28 <torajiro282828@gmail.com>
2023-03-09 21:47:20 +0900

    追加: supabaseでDBホストするためのコード追加

--- backend/api/db.js
+++ backend/api/db.js
@@ -1,11 +1,13 @@
 const Pool = require("pg").Pool;
+const connectionString = "postgres://postgres:haijn238jmkval@db.onnpikzsjidabywbmuna.supabase.co:6543/treedb"
 
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "treedb",
-    password: "Torajirolove1!",
-    port: 5432,
+    connectionString,
+    // user: "postgres",
+    // host: "localhost",
+    // database: "treedb",
+    // password: "Torajirolove1!",
+    // port: 5432,
 })
 
 module.exports = pool;
