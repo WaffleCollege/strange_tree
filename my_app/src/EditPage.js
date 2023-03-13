55d19e747645c1f6d1a600b70e55ddf1fa8845c7
nachan28 <torajiro282828@gmail.com>
2023-03-13 19:26:36 +0900

    追加: EditPageにコンテンツを追加

--- my_app/src/EditPage.js
+++ my_app/src/EditPage.js
@@ -1,11 +1,20 @@
 import React from "react";
 import ItemBox from "./ItemBox";
+import Tree from "./Tree";
 
-const EditPage = () => {
+
+const EditPage = (repoNames, setRepoNames, commits, setCommits, token) => {
   return (
     <>
       <div>EditPage</div>
       <ItemBox />
+      <Tree
+        repoNames={repoNames}
+        setRepoNames={setRepoNames}
+        commits={commits}
+        setCommits={setCommits}
+        token={token}
+      />
     </>
   );
 };
