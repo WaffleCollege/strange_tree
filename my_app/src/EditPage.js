2df5d1f22383faba4891976954a6fdb5352080e7
nachan28 <torajiro282828@gmail.com>
2023-03-15 00:50:37 +0900

    追加: react-draggableでアイテムをdraggableにした

--- my_app/src/EditPage.js
+++ my_app/src/EditPage.js
@@ -4,10 +4,35 @@ import Tree from "./Tree";
 import Draggable from "react-draggable";
 
 const EditPage = () => {
+  const [activeDrags, setActiveDrags] = useState(0);
+  const [deltaPosition, setDeltaPosition] = useState({ x: 0, y: 0 });
+  const [controlledPosition, setControlledPosition] = useState({ x: 0, y: 0 });
+  const handleDrag = (e, ui) => {
+    const { x, y } = deltaPosition;
+    setDeltaPosition({
+      x: x + ui.deltaX,
+      y: y + ui.deltaY,
+    });
+  };
+
+  const onStart = () => {
+    setActiveDrags((prev) => prev + 1);
+  };
+
+  const onStop = () => {
+    setActiveDrags((prev) => prev - 1);
+  };
+
+  const dragHandlers = { onStart: onStart, onStop: onStop };
+  // const imageUrl = "../public/fruit_ringo.png";
   return (
     <div className="container">
       <ItemBox>
-        <img src="./fruit_ringo.png" alt="apple" className="itemImg" />
+        <Draggable onDrag={handleDrag} {...dragHandlers}>
+          <div className="box">
+            <img onDragStart={e => e.preventDefault()} src="./fruit_ringo.png" alt="apple" className="itemImg" />
+          </div>
+        </Draggable>
       </ItemBox>
       <Tree />
     </div>
