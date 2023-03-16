9fd6731bcb1fcfd4fc03f2f62bcabd3567292024
nachan28 <torajiro282828@gmail.com>
2023-03-16 13:13:40 +0900

    追加: MyPage.cssを追加

--- my_app/src/MyPage.js
+++ my_app/src/MyPage.js
@@ -5,6 +5,7 @@ import Button from "./Button.js";
 import Sun from "./Sun.js";
 import WeeklyCommit from "./WeeklyCommit.js";
 import { useAuthContext } from "./context.js";
+import "./MyPage.css";
 
 const MyPage = () => {
   const { isAuth } = useAuthContext();
@@ -20,12 +21,14 @@ const MyPage = () => {
   }, [isAuth]);
 
   return (
-    <>
+    <div className="myPageContainer">
       <Sun />
-      <Tree />
-      <Button function={move} text="ステージ一覧" />
-      <WeeklyCommit />
-    </>
+      <div className="treeContents">
+        <Tree />
+        <Button function={move} text="ステージ一覧" />
+        <WeeklyCommit />
+      </div>
+    </div>
   );
 };
 
